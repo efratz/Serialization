1a7a41b1884558b455ec53fea9c65d11699c64cf
efratz <efrat0548525405@gmail.com>
2022-06-03 14:01:52 +0300

    Add project files.

---
+++ Lehava.Persons.Entities/PersonEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Lehava.Persons.Entities
+{
+    public class PersonEntity //: Model.Person
+    {
+        System.Collections.Generic.List<Model.Person> persons = new List<Model.Person>();
+        Model.Person p;
+        // Saved in UI project - Startup Project
+        // string path = @"persons.txt";
+        // Saved in current project
+        string path = @".\..\..\..\Lehava.Persons.Entities\bin\Debug\persons.txt";
+
+        public void AddRecord(string name, int age)
+        {
+            p = new Model.Person();
+            p.Name = name;
+            p.Age = age;
+            persons.Add(p);
+        }
+
+        // Serialization
+        public void SaveAll()
+        {
+            string str = JsonSerializer.Serialize(persons);
+            File.WriteAllText(path, str);
+        }
+
+        // Deserialization
+        public void Load()
+        {
+            string loadStr = File.ReadAllText(path);
+            Model.Person[] loadPersons = JsonSerializer.Deserialize<Model.Person[]>(loadStr);
+        }
+    }
+}
---
+++ Lehava.Persons/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lehava.Persons.Entities;
+
+namespace Lehava.Persons
+{
+    public partial class Form1 : Form
+    {
+        PersonEntity entity;
+
+        public Form1()
+        {
+            InitializeComponent();
+            entity = new PersonEntity();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            entity.AddRecord(textBox1.Text, int.Parse(textBox2.Text));
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            entity.SaveAll();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            entity.Load();
+        }
+    }
+}
